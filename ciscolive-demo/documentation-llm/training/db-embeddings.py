22f82eebed5f6e384f1eee96be05a067d2f6e183
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2024-02-07 15:30:25 -0800

    Update db-embeddings.py

--- ciscolive-demo/documentation-llm/training/db-embeddings.py
+++ ciscolive-demo/documentation-llm/training/db-embeddings.py
@@ -6,7 +6,7 @@ from sentence_transformers import SentenceTransformer
 from psycopg2 import sql
 import re
 
-def create_database(db_name, user, password, host="db", port="5432"):
+def create_database(db_name, user, password, host="localhost", port="5433"):
     '''
     Creates a database if one has not been created yet
     For initialization
@@ -33,8 +33,8 @@ db_params = {
     'dbname': 'cisco_embeddings',
     'user': 'postgres',
     'password': 'secret',
-    'host': 'db',
-    'port': '5432'
+    'host': 'localhost',
+    'port': '5433'
 }
  
 def read_text_file(file_path):
@@ -167,7 +167,7 @@ def insert_into_db(data):
             print("Successfully inserted embeddings into DB")
 
 def main():
-    create_database('cisco_embeddings', 'postgres', 'secret', 'db', '5432')
+    create_database('cisco_embeddings', 'postgres', 'secret', 'localhost', '5433')
 
     # 1. Segment the file content
     data = segment_file('./cleaned_file.txt')
