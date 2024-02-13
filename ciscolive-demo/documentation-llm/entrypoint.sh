55adf7380b97cae518aefd8748dbd58f34524b35
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2024-02-12 16:01:02 -0800

    Fix Issues With Docker

--- ciscolive-demo/documentation-llm/entrypoint.sh
+++ ciscolive-demo/documentation-llm/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 pip install -r /app/requirements.txt
 
+# Get modelfile
+wget https://huggingface.co/TheBloke/rocket-3B-GGUF/resolve/main/rocket-3b.Q4_K_M.gguf -O /app/backend/llm/model.gguf
+
 # Navigate to training directory and run scripts
 cd /app/training
 python pdf.py
